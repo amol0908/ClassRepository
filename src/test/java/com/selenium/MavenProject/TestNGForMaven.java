c1c8acdce5c8b617a9a99cde17d6b6533ddde5b7
amol0908 <amolsjadhav09@gmail.com>
2018-06-09 10:29:13 +0530

    Amol First Commit

---
+++ src/test/java/com/selenium/MavenProject/TestNGForMaven.java
@@ -0,0 +1,24 @@
+package com.selenium.MavenProject;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class TestNGForMaven {
+  
+	
+	
+	@Test
+  public void f() {
+		
+		System.out.println("Amol Selenium !!!");
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
